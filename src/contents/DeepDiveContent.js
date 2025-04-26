fd669af652d4eef6addfd10b5d8c372d65419af4
ahitagnied <ad158@rice.edu>
2025-04-26 17:57:34 -0500

    add deep-dive content json

---
+++ src/contents/DeepDiveContent.js
@@ -0,0 +1,16 @@
+export const deepdivecont = [
+    {
+      title: "Deep Learning",
+      description: "Deep dive on deep learning with a focus on computer vision and imaging, building up from basic DNNs to INRs, NeRFs, & GAN-Diffusion Hybrids",
+      link: "https://github.com/ahitagnied/deep-learning",
+      github: "ahitagnied/deep-learning"
+    },
+    {
+      title: "RasterRascal",
+      description: "Minimal GPU pipeline implementing vertex processing, rasterization, fragment shading, and frame composition.",
+      link: "https://github.com/ahitagnied/",
+      github: "ahitagnied/coming-soon"
+    },
+  ];
+  
+  export default deepdivecont;
