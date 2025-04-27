2a5a3d5ce067e95dc38047a32539b0ff802ab736
ahitagnied <ad158@rice.edu>
2025-04-26 19:24:48 -0500

    + contacts & team info

---
+++ src/contents/ContactData.js
@@ -0,0 +1,14 @@
+export const data = {
+    contacts: [
+      { name: "Twitter",   url: "https://x.com/ahitagnied" },
+      { name: "Mail",      url: "mailto:ahitagnis@gmail.com" },
+      { name: "Resume",   url: "https://read.cv/your-profile" },
+      { name: "LinkedIn",  url: "https://www.linkedin.com/in/ahitagnid/" },
+    ],
+    teams: [
+      { name: "MIT Media Lab",    color: "#E1306C", period: "2024 "},
+      { name: "Comp. Img.", color: "#005BBB", period: "2024-" },
+      { name: "ML@Rice",        color: "#000000", period: "2024-"},
+      { name: "Arda",        color: "#0ca5de", period: "2023"},
+    ],
+  };
