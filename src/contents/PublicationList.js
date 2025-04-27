3db2326f4b69f99ea11ebcc037cc26362e57c19c
ahitagnied <ad158@rice.edu>
2025-04-27 13:40:59 -0500

    + publications content

---
+++ src/contents/PublicationList.js
@@ -0,0 +1,12 @@
+export const publist = [
+    {
+        title: "An outlook on sodium-ion battery technology toward practical application",
+        authors: "Mingrui Xu, Xi Chen, Sreehari K. Saju, Ahitagni Das, Atin Pramanik & Pulickel M. Ajayan",
+        link: "https://link.springer.com/article/10.1007/s10853-024-10503-z",
+        coverImage: "pub-covers/outlook.png",
+        journal: "The Journal of Material Sciences, 2024",
+        description: "Comprehensive review analyzes sodium-ion batteries as cost-effective, sustainable alternatives to lithium-ion batteries."
+    },
+  ];
+  
+  export default publist;
