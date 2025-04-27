13acf47a45a565d7fe45a524c50e0280f96f04f7
ahitagnied <ad158@rice.edu>
2025-04-27 13:39:35 -0500

    writings -> pubs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Reading from './pages/Reading';
-import DeepDive from './pages/DeepDive'
+import DeepDive from './pages/DeepDive';
+import Publications from './pages/Publications'
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/reading" element={<Reading />} />
           <Route path="/deep-dives" element={<DeepDive />} />
+          <Route path="/publications" element={<Publications />} />
         </Routes>
       </Layout>
     </Router>
