3f0e1ac0694d97b3937b5bee4c86ef80c32cc607
ahitagnied <ad158@rice.edu>
2025-04-27 23:30:26 -0500

    + limited proj list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
-import denali from '../assets/denali.png'
+import profile from '../assets/profile.jpg'
 import React, { useEffect } from 'react';
 import Contact from "../components/Contact";
-import {data} from '../contents/ContactData';
-import {publist} from '../contents/PublicationList'
+import { data } from '../contents/ContactData';
+import { publist } from '../contents/PublicationList'
 import { PublicationList } from '../components/PublicationList';
+import { ProjectList } from '../components/ProjectList';
+import { projlist } from '../contents/ProjList'
 
 function Home() {
   useEffect(() => {
@@ -13,6 +15,7 @@ function Home() {
   }, []);
 
   const limitedPubList = publist.slice(0, 3);
+  const limitedProjList = projlist.slice(0, 3);
 
   return (
     <div style={{
@@ -22,10 +25,10 @@ function Home() {
     }}>
       
     <div style={{ lineHeight: '1.2', fontSize: '16px', fontFamily: 'Lora' }}>
-        <img src={denali} style={{width: '100%', borderRadius: '20px', marginTop: '20px'}} alt="Banner" />
+        <img src={profile} style={{width: '30%', borderRadius: '50%', marginTop: '20px', marginBottom: '20px', display: 'block', marginLeft: 'auto', marginRight: 'auto'}} alt="Banner" />
         <p>
           Hello! I am Ahitagni (or Ahi), I am an undergrad at Rice University pursuing a dual degree in EE and CS. 
-          I like AI, software, and startups. Enjoy taking pictures and hiking. 
+          I like AI, software, and startups. Enjoy taking pictures, sailing and hiking. 
         </p>
 
 
@@ -49,6 +52,10 @@ function Home() {
         {/* limited publication list */}
         <h4>Publications</h4>
         <PublicationList publist={limitedPubList} isHome={true}/>
+
+        {/* limited projects list */}
+        <h4>Projects</h4>
+        <ProjectList projlist={limitedProjList} isHome={true}/>
       </div>
 
     </div>
