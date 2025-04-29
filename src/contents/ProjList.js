c7ffe042e45ac6f21b4cc821dbcd806a7a8a1664
ahitagnied <ad158@rice.edu>
2025-04-29 12:37:21 -0500

    add code link for korekau, update isef png

--- src/contents/ProjList.js
+++ src/contents/ProjList.js
@@ -21,6 +21,7 @@ export const projlist = [
         title: "Korekau: An Autonomous Rocket Recovery System",
         members: "Aarush Adabala, Beck Edwards & Ahitagni Das",
         paper: "https://drive.google.com/file/d/1Zjdf6vY3b-OrO7W0INVWSGKUUpH65UIx/view",
+        github: "https://github.com/rice-eclipse/auto-recovery/",
         coverImage: "project-covers/korekau.png",
         description: "Autonomous payload descent using GPS/IMU-guided parachute steering with fail-safe spiraling. (RiceU Spaceport America Cup 2024)"
     },
@@ -28,7 +29,7 @@ export const projlist = [
         title: "Electrical Energy Generation through B/N-Gr Extracted from Plastic Waste",
         members: "Ahitagni Das, Dr. Kalyan Raidongia",
         paper: "https://drive.google.com/file/d/1ScYXtU7CZkwQivOd9LTMyl2QigBZypI-/view?usp=sharing",
-        coverImage: "project-covers/plastic.png",
+        coverImage: "project-covers/isef.png",
         description: "120 mV and 0.8 µA from 6g of plastic. 1st Special Award at ISEF 2023 and $60K+ scholarship to King Fahad University."
     },
   ];
--- src/contents/PublicationList.js
+++ src/contents/PublicationList.js
@@ -2,10 +2,9 @@ export const publist = [
     {
         title: "An outlook on sodium-ion battery technology toward practical application",
         authors: "Mingrui Xu, Xi Chen, Sreehari K. Saju, Ahitagni Das, Atin Pramanik & Pulickel M. Ajayan",
-        link: "https://link.springer.com/article/10.1007/s10853-024-10503-z",
+        paper: "https://link.springer.com/article/10.1007/s10853-024-10503-z",
         coverImage: "pub-covers/outlook.png",
         journal: "The Journal of Material Sciences, 2024",
-        description: "Comprehensive review analyzes sodium-ion batteries as cost-effective, sustainable alternatives to lithium-ion batteries."
     },
   ];
   
