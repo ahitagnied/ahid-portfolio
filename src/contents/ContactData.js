78e0f96962134fc97044af6fa76d2f38208593b1
Ahitagni D <157437641+ahitagnied@users.noreply.github.com>
2025-05-19 13:41:00 -0500

    + calendly

--- src/contents/ContactData.js
+++ src/contents/ContactData.js
@@ -1,16 +1,16 @@
 export const data = {
     contacts: [
-      { name: "Twitter",   url: "https://x.com/ahitagnied" },
-      { name: "Mail",      url: "mailto:ahitagnis@gmail.com" },
-      { name: "Resume",   url: "https://read.cv/your-profile" },
-      { name: "LinkedIn",  url: "https://www.linkedin.com/in/ahitagnid/" },
-      { name: "Scholar",   url: "https://scholar.google.com/citations?user=GLTc7LcAAAAJ&hl=en" }
+        { name: "LinkedIn",  url: "https://www.linkedin.com/in/ahitagnid/" },
+        { name: "Calendly",      url: "https://calendly.com/ahitagnid/new-meeting" },
+        { name: "Twitter",   url: "https://x.com/ahitagnied" },
+        { name: "Resume",   url: "https://read.cv/your-profile" },
+        { name: "Scholar",   url: "https://scholar.google.com/citations?user=GLTc7LcAAAAJ&hl=en" }
     ],
     teams: [
-      { name: "MIT Media Lab",    color: "#22282a", period: "2024 "},
-      { name: "Lotus AI", color: "#ee4bf0", period: "2024-" },
-      { name: "ML@Rice",        color: "#6e3fa2", period: "2024-"},
-      { name: "Comp. Imaging",        color: "#29276c", period: "2024"},
-      { name: "Arda",        color: "#0ca5de", period: "2023"},
+        { name: "MIT Media Lab",    color: "#22282a", period: "2024 "},
+        { name: "Lotus AI", color: "#ee4bf0", period: "2024-" },
+        { name: "ML@Rice",        color: "#6e3fa2", period: "2024-"},
+        { name: "Comp. Imaging",        color: "#29276c", period: "2024"},
+        { name: "Arda",        color: "#0ca5de", period: "2023"},
     ],
   };
