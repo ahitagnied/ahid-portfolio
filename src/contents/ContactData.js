970092f682e80d5a31c5ee8bd38136715bb49f87
Ahitagni D <157437641+ahitagnied@users.noreply.github.com>
2025-05-29 16:01:27 -0500

    update ContactData.js

--- src/contents/ContactData.js
+++ src/contents/ContactData.js
@@ -7,10 +7,10 @@ export const data = {
         { name: "Scholar",   url: "https://scholar.google.com/citations?user=GLTc7LcAAAAJ&hl=en" }
     ],
     teams: [
+        { name: "Sieve",        color: "#ee4bf0", period: "2025-"},
         { name: "MIT Media Lab",    color: "#22282a", period: "2024 "},
-        { name: "Lotus AI", color: "#ee4bf0", period: "2024-" },
-        { name: "ML@Rice",        color: "#6e3fa2", period: "2024-"},
-        { name: "Comp. Imaging",        color: "#29276c", period: "2024"},
-        { name: "Arda",        color: "#0ca5de", period: "2023"},
+        { name: "Lotus AI", color: "#6e3fa2", period: "2024-" },
+        { name: "Comp. Imaging",        color: "#29276c", period: "2024-"},
+        { name: "ML@Rice",        color: "#0ca5de", period: "2024-"},
     ],
   };
