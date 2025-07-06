91e5e1e1624913438b3ba53060d9ab0395ed8ed2
ahitagnied <ad158@rice.edu>
2025-07-06 15:20:52 -0700

    rm pics

--- src/contents/ProjList.js
+++ src/contents/ProjList.js
@@ -1,36 +1,31 @@
 export const projlist = [
     {
-        title: "BabyGPT: A Frugal Implementation of ChatGPT-2 with Triton Kernels",
+        title: "BabyGPT",
         github: "https://github.com/ahitagnied/babygpt",
-        coverImage: "project-covers/attention.png",
-        description: "GPT-2 with transformers, causal attention, GELU; Triton-optimized extension planned."
+        description: "A Frugal Implementation of ChatGPT-2 with Triton Kernels"
     },
     {
-        title: "Klix: Real-Time AI Voice Calling Agent & Evaluation System",
+        title: "Klix",
         github: "https://github.com/ahitagnied/klix-ai",
-        coverImage: "project-covers/klix.png",
-        description: "Real-time calling, bug detection, and AI feedback via Deepgram, Cartesia, Pipecat, Twilio, OpenAI, and other APIs."
+        description: "Real-Time AI Voice Calling Agent & Evaluation System"
     },
     {
-        title: "Hyperspectral Imaging Payload for Crop Analysis",
+        title: "HSI Camera",
         members: "Ahitagni Das, Ian Schechter",
-        coverImage: "project-covers/hyperspectral.png",
         description: "3U hyperspectral imager with sensor-triggered capture and onboard crop analysis."
     },
     {
-        title: "Korekau: An Autonomous Rocket Recovery System",
+        title: "Korekau",
         members: "Aarush Adabala, Beck Edwards & Ahitagni Das",
         paper: "https://drive.google.com/file/d/1Zjdf6vY3b-OrO7W0INVWSGKUUpH65UIx/view",
         github: "https://github.com/rice-eclipse/auto-recovery/",
-        coverImage: "project-covers/korekau.png",
-        description: "Autonomous payload descent using GPS/IMU-guided parachute steering with fail-safe spiraling. (RiceU Spaceport America Cup 2024)"
+        description: "An Autonomous GPS/IMU-guided Rocket Recovery System"
     },
     {
-        title: "Electrical Energy Generation through B/N-Gr Extracted from Plastic Waste",
+        title: "B/N-Gr WTE",
         members: "Ahitagni Das, Dr. Kalyan Raidongia",
         paper: "https://drive.google.com/file/d/1ScYXtU7CZkwQivOd9LTMyl2QigBZypI-/view?usp=sharing",
-        coverImage: "project-covers/isef.png",
-        description: "120 mV and 0.8 µA from 6g of plastic. 1st Special Award at ISEF 2023 and $60K+ scholarship to King Fahad University."
+        description: "Plastic waste to energy using a novel B/N-Gr WTE reactor",
     },
   ];
   
