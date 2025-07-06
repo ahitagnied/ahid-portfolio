ae9513c3224a52539fed5c32450ddddde8361d95
ahitagnied <ad158@rice.edu>
2025-07-06 15:23:39 -0700

    + gs ds

--- src/contents/ProjList.js
+++ src/contents/ProjList.js
@@ -1,5 +1,10 @@
 export const projlist = [
     {
+        title: "3D-GS Dataset Generator",
+        github: "https://github.com/ahitagnied/gs-dataset",
+        description: "NeRO/ ShinyBlenderSynthetic Dataset Generator for 3D Gaussian Splatting",
+    },
+    {
         title: "BabyGPT",
         github: "https://github.com/ahitagnied/babygpt",
         description: "A Frugal Implementation of ChatGPT-2 with Triton Kernels"
@@ -8,11 +13,6 @@ export const projlist = [
         title: "Klix",
         github: "https://github.com/ahitagnied/klix-ai",
         description: "Real-Time AI Voice Calling Agent & Evaluation System"
-    },
-    {
-        title: "HSI Camera",
-        members: "Ahitagni Das, Ian Schechter",
-        description: "3U hyperspectral imager with sensor-triggered capture and onboard crop analysis."
     },
     {
         title: "Korekau",
@@ -26,6 +26,11 @@ export const projlist = [
         members: "Ahitagni Das, Dr. Kalyan Raidongia",
         paper: "https://drive.google.com/file/d/1ScYXtU7CZkwQivOd9LTMyl2QigBZypI-/view?usp=sharing",
         description: "Plastic waste to energy using a novel B/N-Gr WTE reactor",
+    },
+    {
+        title: "HSI Camera",
+        members: "Ahitagni Das, Ian Schechter",
+        description: "3U hyperspectral imager with sensor-triggered capture and onboard crop analysis."
     },
   ];
   
