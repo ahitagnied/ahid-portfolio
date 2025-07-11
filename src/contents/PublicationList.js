af6097aab5ebedb5abd7fb93475cd9a9e2b51cdc
ahitagnied <ahi@sievedata.com>
2025-07-11 16:16:43 -0700

    update img

--- src/contents/PublicationList.js
+++ src/contents/PublicationList.js
@@ -3,7 +3,7 @@ export const publist = [
         title: "An outlook on sodium-ion battery technology toward practical application",
         authors: "Mingrui Xu, Xi Chen, Sreehari K. Saju, Ahitagni Das, Atin Pramanik & Pulickel M. Ajayan",
         paper: "https://link.springer.com/article/10.1007/s10853-024-10503-z",
-        coverImage: "pub-covers/outlook.png",
+        coverImage: "pub-covers/ajayan.png",
         journal: "The Journal of Material Sciences, 2024",
     },
   ];
