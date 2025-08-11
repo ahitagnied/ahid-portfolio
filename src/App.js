852f1b9526cf600fe068eaf7a3500b6f5bb77b33
ahitagnied <ad158@rice.edu>
2025-08-10 22:11:16 -0700

    fix: rm deep dive

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Reading from './pages/Reading';
-import DeepDive from './pages/DeepDive';
 import Publications from './pages/Publications';
 import Projects from './pages/Projects';
 import './App.css';
@@ -16,7 +15,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/reading" element={<Reading />} />
-          <Route path="/deep-dives" element={<DeepDive />} />
           <Route path="/publications" element={<Publications />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -91,7 +91,6 @@ function Navbar() {
           <NavLink to="/reading">Reading</NavLink>
           <NavLink to="/projects">Projects</NavLink>
           <NavLink to="/publications">Publications</NavLink>
-          <NavLink to="/deep-dives">Deep Dives</NavLink>
         </div>
         
         <div>
@@ -163,7 +162,6 @@ function Navbar() {
         >
           <div style={{ marginBottom: '20px' }}>
             <NavLink to="/publications">Publications</NavLink>
-            <NavLink to="/deep-dives">Deep Dives</NavLink>
             <NavLink to="/projects">Projects</NavLink>
           </div>
           
