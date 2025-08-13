dea212545d654724b85cda703673bf140897c884
ahitagnied <ahi@sievedata.com>
2025-08-12 21:33:39 -0700

    rm pub

--- src/contents/PublicationList.js
+++ src/contents/PublicationList.js
@@ -1,12 +1,5 @@
 export const publist = [
     {
-        title: "Enhancing the Performance of Li/Na-Ion Batteries with Hexagonal Boron Nitride: Advances and Opportunities",
-        authors: "Ahitagni Das, Atin Pramanik, Mingrui Xu, Xinting Shuai, Abhijit Biswas, Robert Vajtai, Pulickel M. Ajayan",
-        paper: "https://ahis.me/publications",
-        coverImage: "pub-covers/hbn.jpg",
-        journal: "Coming Soon, 2025",
-    },
-    {
         title: "An outlook on sodium-ion battery technology toward practical application",
         authors: "Mingrui Xu, Xi Chen, Sreehari K. Saju, Ahitagni Das, Atin Pramanik & Pulickel M. Ajayan",
         paper: "https://link.springer.com/article/10.1007/s10853-024-10503-z",
