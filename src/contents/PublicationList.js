978f45f1df1a7a96f5ed1743f9e298581a3fb75b
ahitagnied <ad158@rice.edu>
2025-09-25 17:18:22 -0500

    new pub

--- src/contents/PublicationList.js
+++ src/contents/PublicationList.js
@@ -1,5 +1,12 @@
 export const publist = [
     {
+        title: "Enhancing the Performance of Li/Na-Ion Batteries with Hexagonal Boron Nitride",
+        authors: "Ahitagni Das, Atin Pramanik, Mingrui Xu, Xinting Shuai, Abhijit Biswas, Robert Vajtai, Pulickel M. Ajayan",
+        paper: "https://pubs.acs.org/doi/abs/10.1021/acsnano.5c11425",
+        coverImage: "pub-covers/hbn.png",
+        journal: "ACS Nano, 2025",
+    },
+    {
         title: "An outlook on sodium-ion battery technology toward practical application",
         authors: "Mingrui Xu, Xi Chen, Sreehari K. Saju, Ahitagni Das, Atin Pramanik & Pulickel M. Ajayan",
         paper: "https://link.springer.com/article/10.1007/s10853-024-10503-z",
